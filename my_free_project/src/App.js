594ef66f137f5c4d29e0702dd8a6f8827edb9d89
Gabriel_Q <hqaddaha44@icloud.com>
2024-02-02 10:23:23 +0100

    validation avant basculement

---
+++ my_free_project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import './css/dashboard.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className='body'>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
