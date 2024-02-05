fc3b01c504a1c805cf6014f679007ba91ffb473d
Gabriel_Q <hqaddaha44@icloud.com>
2024-02-05 20:46:23 +0100

    component app et autres

--- my_free_project/src/App.js
+++ my_free_project/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './index.css';
 import './css/dashboard.css';
 import Dashboard from './components/Dashboard';
+import Profil from './components/Profil';
 
 function App() {
   return (
     <div className='body'>
+      <h1 className='titre'>Mon super site</h1>
       <Dashboard />
+      {/* <Profil /> */}
     </div>
   );
 }
